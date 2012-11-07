cbe441983e269a8bc15e2201cac99b14e04ab376
Michael Lawson <mdlawson@gmx.com>
2012-11-07 19:05:18 +0000

    moving character

---
+++ main.c
@@ -0,0 +1,59 @@
+#include <ncurses.h>
+#include <unistd.h>
+
+typedef struct {
+	int x;
+	int y;
+} block;
+
+block player = {0,0};
+
+void main(){
+	initscr();
+	cbreak();
+	nodelay(stdscr, TRUE);
+	keypad(stdscr, TRUE);
+	noecho();
+	curs_set(0);
+	int step = 30*1000;
+	int in;
+	int running = 1;
+	while (running) {
+		usleep(step);
+		in = getch();
+		switch(in){
+			case 'q':
+				running = 0;
+				break;
+			case KEY_UP:
+				player.y--;
+				break;
+			case KEY_DOWN:
+				player.y++;
+				break;
+			case KEY_RIGHT:
+				player.x++;
+				break;
+			case KEY_LEFT:
+				player.x--;
+				break;
+			default:
+				break;
+		}
+		update();
+		render();
+	}
+	endwin();
+	return;
+}
+
+void render(){
+	clear();
+	mvaddch(player.y,player.x,'@');
+	refresh();
+}
+
+void update(){
+
+}
+
